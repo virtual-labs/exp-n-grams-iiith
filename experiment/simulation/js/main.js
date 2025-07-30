1da09c73414041f37acb3bc5cc543a9700bb4229
Shraddha <shraddha.more@gmail.com>
2025-07-30 11:07:42 +0530

    Fixing the path
    Fixing data path

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -80,8 +80,8 @@ class CorpusManager {
       if (file.endsWith(".jpg") || file.endsWith(".png")) {
         sentencesDiv.innerHTML = `<img src="/data/corpora/${file}" alt="${file}" style="max-width:100%;" />`;
       } else {
-        // const response = await fetch(`/data/corpora/${file}`);
-        const response = await fetch(`/simulation/data/corpora/${file}`);
+        const response = await fetch(`/data/corpora/${file}`);
+        // const response = await fetch(`/simulation/data/corpora/${file}`);
         if (!response.ok) throw new Error("File not found");
         const text = await response.text();
         // Expecting corpus, bigrams, order separated by '&' (as in PHP)
